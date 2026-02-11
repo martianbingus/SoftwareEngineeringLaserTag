7bb138db106dc5bfc7c6bf4db6180e1b0a5a1c85
Bright <brightrupp@gmail.com>
2026-02-11 07:37:44 -0600

    Wrote the class code for Database

--- Database.java
+++ Database.java
@@ -1,3 +1,112 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+//import java.sql.Driver;
+//import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//developed by Bright Rupp, the one and only :3 UwU
+
 public class Database {
-    
+    private Connection connection;
+
+    //for dumb reasons, I couldn't handle the SQLException in the constructor, so you'll need to use try-catch when you call Database()
+    Database() throws SQLException
+    {
+        this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/photon", "student", "student");
+    }
+
+    //verifies that a given id is listed on the database
+    public boolean checkForId(int id)
+    {
+        String qs = "SELECT id, codename FROM players WHERE id = ?";
+        try (PreparedStatement query = connection.prepareStatement(qs))
+        { //apparently try statements automatically close these resources when they're in parenthesis?
+            query.setInt(1, id);
+            try (ResultSet r = query.executeQuery())
+            {
+                return r.next();
+            }
+        }
+        catch (SQLException UwU) {
+            UwU.printStackTrace();
+            return false;
+        }
+    }
+
+    //takes a player id, returns the corresponding codename on the database
+    //returns null if id does not exist
+    //thus you might use queryId() first to make sure the id is there
+    public String getCodename(int id) throws SQLException
+    {
+        
+        String qs = "SELECT codename FROM players WHERE id = ?";
+        try (PreparedStatement query = connection.prepareStatement(qs))
+        {
+            query.setInt(1, id);
+            try (ResultSet r = query.executeQuery())
+            {
+                if (r.next())
+                {
+                    return r.getString("codename");
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        catch (SQLException UwU) {
+            UwU.printStackTrace();
+            return null;
+        }
+        
+    }
+
+    //pretty self-explanatory tbh, pass in two values and add them to the database lmfao
+    public void insertEntry(int id, String codename)
+    {
+        
+        String qs = "INSERT INTO players (id, codename) VALUES (?, ?)";
+        try (PreparedStatement query = connection.prepareStatement(qs))
+        {
+            query.setInt(1, id);
+            query.setString(2, codename);
+            query.executeUpdate();
+        }
+        catch (SQLException UwU) {
+            UwU.printStackTrace();
+        }
+    }
+
+    //delete an entry using its id
+    public void deleteEntryById(int id)
+    {
+        String qs = "DELETE FROM players WHERE id = ?";
+        try (PreparedStatement query = connection.prepareStatement(qs))
+        {
+            query.setInt(1, id);
+            query.executeUpdate();
+        }
+        catch (SQLException UwU) {
+            UwU.printStackTrace();
+        }
+    }
+
+    //delete an entry using its codename (could technically delete multiple
+    //  players at once if they share a codename but I added this anyway in
+    //  case you found it necessary)
+    public void deleteEntryByCodename(String codename)
+    {
+        String qs = "DELETE FROM players where codename = ?";
+        try (PreparedStatement query = connection.prepareStatement(qs))
+        {
+            query.setString(1, codename);
+            query.executeUpdate();
+        }
+        catch (SQLException UwU) {
+            UwU.printStackTrace();
+        }
+    }
 }
